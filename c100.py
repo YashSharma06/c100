2eab0dc86a9562f9e1458bf480856f516e794ce5
YashSharma06 <68421900+YashSharma06@users.noreply.github.com>
2021-10-31 16:58:48 +0530

    Add files via upload

---
+++ c100.py
@@ -0,0 +1,12 @@
+class House(object):
+    def __init__(self, width, height):
+        self.width = width,
+        self.height = height
+    def Colour(self):
+        print("House")
+    def Size(self):
+        print("Colour")
+
+house1 = House( 10, 10)
+house1.Colour
+  
